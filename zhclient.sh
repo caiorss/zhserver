46fc0dab9bba2d3f2e0b7129b11dfab28d9aab43
CaioRS <caiorss.rodrigues@gmail.com>
2017-02-20 12:28:25 -0300

    Refactor zhclient load script.

--- zhclient.sh
+++ zhclient.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/sh
 
-export ZOTERO_DB="sqlite:///home/arch/.mozilla/firefox/mwad0hks.zotero/zotero/zotero2.sqlite"
+export ZOTERO_DB="sqlite://testdb/zotero-db.sqlite"
 export ZOTERO_PATH="/home/arch/.mozilla/firefox/mwad0hks.zotero/zotero/storage/" 
-
 
 
 ./zhclient.bin "$@"
